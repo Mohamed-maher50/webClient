973dd199f4ecaaa531dc4642a49683f9b6f68f2b
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-03-09 09:54:05 +0200

    upload string sheet 4 answer

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-// q 3
-var x = "A+ B+ C+ D+ F+";
-var rep = "",
-  slicer = "",
-  rev = "";
-for (let i = 0; i < x.length; i++) {
-  if (x[i] != "+") rep += x[i];
-  else rep += "-";
-}
-slicer = x.slice(3, 8);
-rev = x.split("").reverse().join("", ",");
-console.log(rep);
-console.log(slicer);
-console.log(rev);
+var x = "The NEW ERA of Sinai University";
+var ahalf = "",
+  bhalf = "",
+  res = "",
+  count_e = "";
+ahalf = x.slice(0, x.length / 2 + 1);
+bhalf = x.slice(x.length / 2);
+for (let i = 0; i < x.length; i++)
+  if (x[i].toLocaleLowerCase() == "e") count_e++;
+
+res += x.slice(15, 16);
+res += x.slice(21, 22);
+console.log(ahalf);
+console.log(bhalf);
+console.log(res);
+console.log(count_e);
