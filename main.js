9926a7c85ddb3ea1dbb25219ec0ec5a50c74b155
mohamed maher <mohamedmaher.mm330@gmail.com>
2023-03-09 09:38:21 +0200

    upload sheet string question 2

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
-var x = "The Price of 1 egg is 7 EGP";
-var res = 0,
-  cas,
-  up = 0,
-  down = 0,
-  collect = "";
-for (let i = 0; i < x.length; i++) if (!isNaN(x[i])) res += +x[i];
-cas = res + x.length;
-
-for (let i = 0; i < x.length; i++)
-  if (x[i] == x[i].toUpperCase() && x[i] != " " && isNaN(x[i])) up++;
-// author answer
-// check ascii table
-// for (let i = 0; i < x.length; i++) {
-//   var c = x[i].charCodeAt();
-//   if (c > 59 && c < 91) up++;
-// }
-for (let i = 0; i < x.length; i++)
-  if (x[i] == x[i].toLowerCase() && x[i] != " " && isNaN(x[i])) down++;
-for (let i = 0; i < x.length; i++)
-  if (!isNaN(x[i]) && x[i] != " ") collect += x[i];
-console.log(res);
-console.log(cas);
-console.log(up);
-console.log(down);
-console.log(collect);
+// second question
+var x = "In 2023 Goodness Comes After Suffering 7P";
+var con = "",
+  lword = "",
+  mid = "",
+  sec = "";
+con = x[0] + x[x.length - 1];
+// or con = x[0] + x.at(-1);
+for (let i = 0; i < x.length; i++) if (x[i] == " ") lword += x[i + 1];
+mid = x.split(" ");
+mid = mid[Math.floor(mid.length / 2)];
+sec = x.split(" ")[1];
+console.log(con);
+console.log(lword);
+console.log(mid);
+console.log(sec);
